74640ddcde2d04ce351a60c98b0a6c1d6b341c09
Ashish Sarker <myselfashishsarker@gmail.com>
2024-08-19 01:24:39 +0600

    mongodb prefectly connected

--- server/index.js
+++ server/index.js
@@ -1,12 +1,43 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const port = process.env.PORT || 5000;
 const app = express();
+
+const uri = `mongodb+srv://${process.env.APP_NAME}:${process.env.APP_PASS}@mongo-app.q62fegx.mongodb.net/?retryWrites=true&w=majority&appName=mongo-app`;
 
 // middleware
 app.use(cors());
 app.use(express.json());
 // middleware
+
+// tea-stall
+// SyHFFiM7N48ScJFL
+
+// mongodb
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+        // Send a ping to confirm a successful connection
+        // await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+// mongodb
 
 app.get("/", (req, res) => {
     res.send("server is running");
