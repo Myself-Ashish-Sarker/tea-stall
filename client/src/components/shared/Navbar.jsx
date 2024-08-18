abc66517a82bcb467e1166423fdbf949bd6fb24e
Ashish Sarker <myselfashishsarker@gmail.com>
2024-08-18 17:43:02 +0600

    navbar fully on track, more routes might need to add but later, app can track the routing path

--- client/src/components/shared/Navbar.jsx
+++ client/src/components/shared/Navbar.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
 const Navbar = () => {
+
+    const location = useLocation();
+    const pathname = location.pathname;
+    console.log(pathname); 
+
+    const links = [
+        {
+            title: "Home",
+            path: "/"
+        },
+        {
+            title: "About",
+            path: "/about"
+        }
+    ]
+
     return (
         <div>
-            <div className="navbar bg-base-100">
+            <div className="navbar bg-[#900C3F]">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -23,36 +40,22 @@ const Navbar = () => {
                         <ul
                             tabIndex={0}
                             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                            <li><a>Item 1</a></li>
-                            <li>
-                                <a>Parent</a>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </li>
-                            <li><a>Item 3</a></li>
+                            {
+                                links.map(link => <li key={link.title}><Link to={link.path}>{link.title}</Link></li>)
+                            }
                         </ul>
                     </div>
-                    <a className="btn btn-ghost text-xl">daisyUI</a>
+                    <a className="btn btn-ghost text-xl text-white">tea-stall</a>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
-                        <li><a>Item 1</a></li>
-                        <li>
-                            <details>
-                                <summary>Parent</summary>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </details>
-                        </li>
-                        <li><a>Item 3</a></li>
+                        {
+                            links.map(link => <li key={link.title}><Link className='text-white' to={link.path} >{link.title}</Link></li>)
+                        }
                     </ul>
                 </div>
                 <div className="navbar-end">
-                    <a className="btn">Button</a>
+                    <Link className="btn">Login</Link>
                 </div>
             </div>
         </div>
