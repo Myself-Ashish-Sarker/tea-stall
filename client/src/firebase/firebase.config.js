c24ef6f2a47ad1a90ddf30eaef25643ed6c9754e
Ashish Sarker <myselfashishsarker@gmail.com>
2024-08-18 20:26:53 +0600

    'AuthProvider.jsx' created but not used

--- client/src/firebase/firebase.config.js
+++ client/src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBPKj31JSajq3LmhmbJ9j63NyyumFx-b1s",
-  authDomain: "authentication-app-b7140.firebaseapp.com",
-  projectId: "authentication-app-b7140",
-  storageBucket: "authentication-app-b7140.appspot.com",
-  messagingSenderId: "455355288650",
-  appId: "1:455355288650:web:cff27642f2f9a856d5aab3"
+    apiKey: "AIzaSyBPKj31JSajq3LmhmbJ9j63NyyumFx-b1s",
+    authDomain: "authentication-app-b7140.firebaseapp.com",
+    projectId: "authentication-app-b7140",
+    storageBucket: "authentication-app-b7140.appspot.com",
+    messagingSenderId: "455355288650",
+    appId: "1:455355288650:web:cff27642f2f9a856d5aab3"
 };
 
 // Initialize Firebase
