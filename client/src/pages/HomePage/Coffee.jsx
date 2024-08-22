d3f01174bfae4854d4e8243c08b3ee6d86ed1aa6
Ashish Sarker <myselfashishsarker@gmail.com>
2024-08-22 21:56:00 +0600

    added style in Coffee.jsx

--- client/src/pages/HomePage/Coffee.jsx
+++ client/src/pages/HomePage/Coffee.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import useAxiosPublic from "../../hooks/useAxiosPublic";
+import { GiCoffeeCup } from "react-icons/gi";
 
 const Coffee = () => {
 
@@ -20,28 +21,40 @@ const Coffee = () => {
     }, [])
 
     return (
-        <div className="grid grid-cols-3 place-items-center gap-5">
-            {
-                coffees.map((coffee, index) => (
-                    <div key={index} className="card bg-base-100 w-96 shadow-xl">
-                        <figure>
-                            <img
-                                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                                alt="Shoes" />
-                        </figure>
-                        <div className="card-body">
-                            <h2 className="card-title">{coffee.coffee_name}</h2>
-                            <p>{coffee.coffee_short_details}</p>
-                            <div className="card-actions justify-start">
-                                <Link to={`/coffee/${coffee._id}`}>
-                                    <button className="btn btn-primary">Coffee Details</button>
-                                </Link>
+        <>
+            <div className="flex justify-center items-center">
+                <GiCoffeeCup className="text-xl" />
+                <h1 className="text-xl text-[#604731] font-semibold">Popular Menu</h1>
+            </div>
+            <div className="mt-2 pb-5 flex justify-center">
+                <h1 className="text-4xl font-extrabold">Our Special Tea & Coffee <span className="text-[#3a2306] font-medium">Menus</span></h1>
+            </div>
+            <div className="grid grid-cols-3 place-items-center gap-5 px-16">
+                {
+                    coffees.map((coffee, index) => (
+                        <div key={index} className="card w-96 shadow-2xl bg-[#f9f6f1]">
+                            <figure>
+                                <img
+                                    className="h-64"
+                                    src={coffee.coffee_img}
+                                    alt={coffee.coffee_name} />
+                            </figure>
+                            <div className="card-body">
+                                <div className="bg-white p-7 rounded-md">
+                                    <h2 className="card-title">{coffee.coffee_name}</h2>
+                                    <p className="text-[#563e29] pb-2">{coffee.coffee_short_details}</p>
+                                    <div className="card-actions justify-start">
+                                        <Link to={`/coffee/${coffee._id}`}>
+                                            <button className="btn bg-[#604731] hover:bg-black text-white">Coffee Details</button>
+                                        </Link>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                ))
-            }
-        </div>
+                    ))
+                }
+            </div>
+        </>
     );
 };
 
--- client/src/pages/HomePage/CoffeeDetails.jsx
+++ client/src/pages/HomePage/CoffeeDetails.jsx
@@ -4,17 +4,17 @@ import { useParams } from "react-router-dom";
 
 const CoffeeDetails = () => {
 
-    const { id } = useParams();  // Use the correct parameter name
+    const { id } = useParams();
     const axiosPublic = useAxiosPublic();
 
     useEffect(() => {
         console.log(`Fetching coffee with ID: ${id}`);
         console.log(`/coffee/${id}`);
 
-        axiosPublic.get(`/coffee/${id}`)  // Use the correct ID in the request
+        axiosPublic.get(`/coffee/${id}`)
             .then(res => {
                 console.log(res.data);
-                setCoffee(res.data);  // Save the data to state
+                setCoffee(res.data);
             })
             .catch(err => {
                 console.log(err.message);
