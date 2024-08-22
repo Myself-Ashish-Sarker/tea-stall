8512929090287b346f4552210eca0de76dd67912
Ashish Sarker <myselfashishsarker@gmail.com>
2024-08-22 22:23:32 +0600

    minimal design applied in coffee details page and admin can't access to add to cart button

--- client/src/pages/HomePage/CoffeeDetails.jsx
+++ client/src/pages/HomePage/CoffeeDetails.jsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react";
 import useAxiosPublic from "../../hooks/useAxiosPublic";
 import { useParams } from "react-router-dom";
+import useAdmin from "../../hooks/useAdmin";
 
 const CoffeeDetails = () => {
 
     const { id } = useParams();
     const axiosPublic = useAxiosPublic();
     const [coffee, setCoffee] = useState(null);
+    const { isAdmin, loading, error } = useAdmin();
 
     useEffect(() => {
         console.log(`Fetching coffee with ID: ${id}`);
@@ -23,16 +25,30 @@ const CoffeeDetails = () => {
     }, [id]);
 
     return (
-        <div>
-            <h1>Coffee Details Page</h1>
-            {
-                coffee && (
-                    <div>
-                        <h1>{coffee.coffee_name}</h1>
-                        <p>{coffee.coffee_details}</p>
-                    </div>
-                )
-            }
+        <div className="bg-[#f9f6f1] min-h-screen p-8">
+            <div className="bg-white p-8 rounded-md">
+                {
+                    coffee && (
+                        <div className="flex gap-6">
+                            <div>
+                                <img className="w-[80rem] rounded-lg" src={coffee.coffee_img} alt={coffee.coffee_name} />
+                            </div>
+                            <div>
+                                <h1 className="text-2xl font-bold">{coffee.coffee_name}</h1>
+                                <p className="pt-4">{coffee.coffee_details}</p>
+                                <div className="pt-8">
+                                    <button
+                                        disabled={isAdmin}
+                                        className=" btn bg-[#604731] hover:bg-black text-white">
+                                            Add to Cart
+                                    </button>
+                                </div>
+                            </div>
+
+                        </div>
+                    )
+                }
+            </div>
         </div>
     );
 };
